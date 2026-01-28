d0698b41138f2cabf443ee025048f24d97030be0
Antony Gitonga <agworld1046@gmail.com>
2026-01-28 22:32:57 +0300

    Added slipstream proto

--- build.rs
+++ build.rs
@@ -2,6 +2,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Compile the slipstream proto file
     tonic_build::configure()
         .build_server(false) // We only need the client
-        .compile(&["../../proto/slipstream.proto"], &["../../proto"])?;
+        .compile(&["proto/slipstream.proto"], &["proto"])?;
     Ok(())
 }
